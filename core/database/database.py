03869d0bddd3946160606fc446d16136dda07218
David Pitta <davidfrpitta@gmail.com>
2023-03-30 19:05:26 -0300

    Primeira entrega

---
+++ core/database/database.py
@@ -0,0 +1,8 @@
+from deta import Deta
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+project_key = os.environ.get('project_key')
+deta = Deta(project_key=project_key)
+db = deta.Base("carrinho")
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 import uvicorn
+from core.database import database as db
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World!"}
+    return {"message": "api em construção"}
+
+@app.get("/carrinho")
+async def carrinho():
+    item = db.db.fetch().items
+    return item
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="localhost", port=8000, reload=True)
