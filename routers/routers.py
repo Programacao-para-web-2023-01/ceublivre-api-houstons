3053562dc8f062c93b7a35476ac5a680c8f94686
David Pitta <davidfrpitta@gmail.com>
2023-04-13 19:37:05 -0300

    Alterando rotas

--- routers/routers.py
+++ routers/routers.py
@@ -10,7 +10,7 @@ async def root():
     return {"message": "api em construção"}
 
 
-@app.get("/carrinho/{user}")
+@app.get("/carrinho/user/{user}")
 async def get_carrinho(user: int):
     item = services.get_carrinho_usuario(user)
     if item:
@@ -19,7 +19,7 @@ async def get_carrinho(user: int):
     raise HTTPException(status_code=404, detail="User does not have an active cart")
 
 
-@app.put("/carrinho/{user}/produto/{prod}")
+@app.put("/carrinho/user/{user}/product/{prod}")
 async def put_qtd_carrinho(user: int, prod: int, carrinho: services.Carrinho):
     if user != carrinho.usuario:
         raise HTTPException(status_code=404, detail="User from path is not equals to body User")
